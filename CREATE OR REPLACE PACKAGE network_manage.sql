860805f804120ad6a77403d4964dca28d22a5e7b
AlbertMLD <albert.lungu@gmail.com>
2023-12-18 00:04:14 +0200

    This PL/SQL package provides functionalities for managing network devices, interfaces, logs, and simulating traffic and alerts in an Oracle Database. The package includes procedures for setting up audit triggers to log actions on specified tables.
    1. [Usage](#usage)
    2. [Procedures](#procedures)
    3. [Table Structure](#table-structure)
    4. [Security Considerations](#security-considerations)
    5. [Testing](#testing)
    6. [Dependencies](#dependencies)
    7. [Contributing](#contributing)
    8. [License](#license)
    
    1. Execute the SQL script to create the `network_management` package and package body in your Oracle Database.
    
        ```sql
        -- Execute the script containing the package and package body
        @your_script.sql
        ```
    
    2. Use the provided procedures to manage network devices, interfaces, logs, and simulate traffic and alerts.
    
    - **`create_and_insert_network_devices`**: Creates a table for network devices and inserts sample data.
    - **`add_device`**: Inserts a new device into the network_devices table.
    - **`create_and_insert_network_interfaces`**: Creates a table for network interfaces and inserts sample data.
    - **`delete_device`**: Deletes a device from the network_devices table based on the provided device_id.
    - **`setup_audit_triggers`**: Sets up audit triggers for specified tables to log insert, update, and delete actions.
    - **`simulate_traffic_and_alerts`**: Simulates network traffic and generates alerts based on traffic conditions.
    - **`create_and_insert_network_logs`**: Creates a table for network logs and inserts sample data.
    
    The package creates the following tables:
    
    - `network_devices`: Stores information about network devices.
    - `network_interfaces`: Stores information about network interfaces.
    - `audit_logs`: Logs actions (insert, update, delete) on specified tables.
    - `trafic_interfete`: Stores simulated network traffic data.
    - `alerte_trafic`: Stores alerts generated based on simulated traffic conditions.
    - `network_logs`: Stores network log data.
    
    - Ensure that input parameters are properly validated to prevent SQL injection.
    - Grant appropriate permissions to the user executing the procedures.
    
    Thoroughly test each procedure in a controlled environment to ensure correct functionality. Pay special attention to error handling and edge cases.
    
    No external dependencies are required. Ensure the Oracle Database environment is properly configured.
    
    If you find issues or have suggestions for improvement, feel free to contribute by opening an issue or submitting a pull request.
    
    This code is licensed under the [MIT License](LICENSE).

--- CREATE OR REPLACE PACKAGE network_manage.sql
+++ CREATE OR REPLACE PACKAGE network_manage.sql
@@ -1,6 +1,17 @@
 CREATE OR REPLACE PACKAGE network_management AS
-    PROCEDURE create_network_devices_table AS
+    PROCEDURE create_and_insert_network_devices;
+    PROCEDURE add_device;
+    PROCEDURE create_and_insert_network_interfaces;
+    PROCEDURE delete_device(p_device_id INT);
+    PROCEDURE setup_audit_triggers(p_table_name VARCHAR2);
+    PROCEDURE simulate_traffic_and_alerts;
+    PROCEDURE create_and_insert_network_logs;
+END network_management;
+/
+CREATE OR REPLACE PACKAGE BODY network_management AS
+    PROCEDURE create_and_insert_network_devices AS
         BEGIN
+            -- Create Table
             BEGIN
                 EXECUTE IMMEDIATE 'CREATE TABLE network_devices (
                     device_id INT PRIMARY KEY,
@@ -15,10 +26,37 @@ CREATE OR REPLACE PACKAGE network_management AS
                 )';
             EXCEPTION
                 WHEN OTHERS THEN
-                    NULL; -- Tabela deja existentă, ignorăm eroarea
+                    NULL; -- Table already exists, ignore the error
             END;
-        END;
-    CREATE OR REPLACE PROCEDURE add_device(
+            -- Insert Sample Data
+            FOR i IN 1..10 LOOP
+                INSERT INTO network_devices (
+                    device_id,
+                    device_name,
+                    device_ip_address,
+                    device_type,
+                    location,
+                    manufacturer,
+                    firmware_version,
+                    status,
+                    last_seen
+                )
+                SELECT
+                    LEVEL,
+                    'Device' || LEVEL,
+                    '192.168.' || TO_CHAR(TRUNC(DBMS_RANDOM.VALUE(1, 255))) || '.' || TO_CHAR(TRUNC(DBMS_RANDOM.VALUE(1, 255))),
+                    CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'Router' ELSE 'Switch' END,
+                    CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'Server Room' ELSE 'Network Closet' END,
+                    'Manufacturer' || LEVEL,
+                    SUBSTR('v' || TO_CHAR(DBMS_RANDOM.VALUE(1, 10)), 1, 10), -- Ensure firmware_version does not exceed 10 characters
+                    CASE WHEN DBMS_RANDOM.VALUE < 0.8 THEN 'Active' ELSE 'Inactive' END,
+                    TO_TIMESTAMP('2023-01-01 09:12', 'YYYY-MM-DD HH24:MI') - DBMS_RANDOM.VALUE(1, 365)
+                FROM DUAL
+                CONNECT BY LEVEL <= 10;
+            END LOOP;
+            COMMIT; -- Commit the changes
+        END create_and_insert_network_devices;
+    PROCEDURE add_device(
             p_device_id INT,
             p_device_name VARCHAR2(50),
             p_device_ip_address VARCHAR2(15),
@@ -41,8 +79,7 @@ CREATE OR REPLACE PACKAGE network_management AS
                 SUBSTR(p_status, 1, 10),
                 p_last_seen;
         END add_device;
-        /
-    CREATE OR REPLACE PROCEDURE create_and_insert_network_interfaces AS
+    PROCEDURE create_and_insert_network_interfaces AS
         BEGIN
             -- Create network_interfaces table
             BEGIN
@@ -58,6 +95,7 @@ CREATE OR REPLACE PACKAGE network_management AS
                 WHEN OTHERS THEN
                     NULL; -- Table already exists, ignore the error
             END;
+
             -- Insert data into network_interfaces table
             INSERT INTO network_interfaces (interface_id, device_id, interface_name, interface_type, speed_mbps, mac_address)
             SELECT
@@ -70,105 +108,104 @@ CREATE OR REPLACE PACKAGE network_management AS
             FROM DUAL
             CONNECT BY LEVEL <= 10;
         END create_and_insert_network_interfaces;
-        /
     PROCEDURE delete_device(p_device_id INT) AS
     	BEGIN
        		EXECUTE IMMEDIATE 'DELETE FROM network_devices WHERE device_id = :1' USING p_device_id;
    	 	END;
-    PROCEDURE insert_data AS
+    PROCEDURE setup_audit_triggers(p_table_name VARCHAR2) AS
         BEGIN
-        -- Adăugarea datelor aleatorii în pachetul network_management
+            -- Create the audit_logs table
+            EXECUTE IMMEDIATE '
+                CREATE TABLE audit_logs (
+                    audit_id INT PRIMARY KEY,
+                    administrator_id INT,
+                    timestamp TIMESTAMP,
+                    action_type VARCHAR2(10),
+                    table_affected VARCHAR2(50),
+                    record_id INT,
+                    details VARCHAR2(4000)
+                )';
 
-        -- Inserarea datelor în tabela network_devices
-            INSERT INTO network_devices (
-                device_id,
-                device_name,
-                device_ip_address,
-                device_type,
-                location,
-                manufacturer,
-                firmware_version,
-                status,
-                last_seen
-            )
-            SELECT
-                LEVEL,
-                'Device' || LEVEL,
-                '192.168.' || TO_CHAR(TRUNC(DBMS_RANDOM.VALUE(1, 255))) || '.' || TO_CHAR(TRUNC(DBMS_RANDOM.VALUE(1, 255))),
-                CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'Router' ELSE 'Switch' END,
-                CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'Server Room' ELSE 'Network Closet' END,
-                'Manufacturer' || LEVEL,
-                SUBSTR('v' || TO_CHAR(DBMS_RANDOM.VALUE(1, 10)), 1, 10), -- Ensure firmware_version does not exceed 10 characters
-                CASE WHEN DBMS_RANDOM.VALUE < 0.8 THEN 'Active' ELSE 'Inactive' END,
-                TO_TIMESTAMP('2023-01-01 09:12', 'YYYY-MM-DD HH24:MI') - DBMS_RANDOM.VALUE(1, 365)
-            FROM DUAL
-            CONNECT BY LEVEL <= 10;
+            -- Create a sequence for audit_id
+            EXECUTE IMMEDIATE '
+                CREATE SEQUENCE audit_id_seq START WITH 1 INCREMENT BY 1';
 
-        -- Inserarea datelor în tabela network_logs
-            INSERT INTO network_logs (log_id, device_id, interface_id, timestamp, log_message, log_level, source_ip)
-            SELECT
-                LEVEL,
-                TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- device_id
-                TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- interface_id
-                TO_TIMESTAMP('2023-01-01 08:05:00', 'YYYY-MM-DD HH24:MI:SS') + DBMS_RANDOM.VALUE(1, 365), -- timestamp
-                'Log Message ' || LEVEL,
-                CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'Info' ELSE 'Error' END,
-                '192.168.' || TO_CHAR(DBMS_RANDOM.VALUE(1, 255)) || '.' || TO_CHAR(DBMS_RANDOM.VALUE(1, 255)) -- source_ip
-            FROM DUAL
-            CONNECT BY LEVEL <= 10;
+            -- Create a trigger to log inserts
+            EXECUTE IMMEDIATE '
+                CREATE OR REPLACE TRIGGER audit_logs_insert_trigger
+                AFTER INSERT ON ' || p_table_name || '
+                FOR EACH ROW
+                BEGIN
+                    INSERT INTO audit_logs (
+                        audit_id,
+                        administrator_id,
+                        timestamp,
+                        action_type,
+                        table_affected,
+                        record_id,
+                        details
+                    ) VALUES (
+                        audit_id_seq.NEXTVAL,
+                        :new.administrator_id,
+                        SYSTIMESTAMP,
+                        ''Insert'',
+                        ''' || p_table_name || ''',
+                        :new.record_id,
+                        ''{"action": "Insert"}''
+                    );
+                END';
 
-        -- Inserarea datelor în tabela bandwidth_data
-            INSERT INTO bandwidth_data (bandwidth_id, interface_id, timestamp, incoming_bandwidth, outgoing_bandwidth)
-            SELECT
-                LEVEL,
-                TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- interface_id
-                TO_TIMESTAMP('2023-01-01 08:10:00', 'YYYY-MM-DD HH24:MI:SS') + DBMS_RANDOM.VALUE(1, 365), -- timestamp
-                TO_NUMBER(DBMS_RANDOM.VALUE(1, 10000)), -- incoming_bandwidth
-                TO_NUMBER(DBMS_RANDOM.VALUE(1, 10000)) -- outgoing_bandwidth
-            FROM DUAL
-            CONNECT BY LEVEL <= 10;
+            -- Create a trigger to log updates
+            EXECUTE IMMEDIATE '
+                CREATE OR REPLACE TRIGGER audit_logs_update_trigger
+                AFTER UPDATE ON ' || p_table_name || '
+                FOR EACH ROW
+                BEGIN
+                    INSERT INTO audit_logs (
+                        audit_id,
+                        administrator_id,
+                        timestamp,
+                        action_type,
+                        table_affected,
+                        record_id,
+                        details
+                    ) VALUES (
+                        audit_id_seq.NEXTVAL,
+                        :new.administrator_id,
+                        SYSTIMESTAMP,
+                        ''Update'',
+                        ''' || p_table_name || ''',
+                        :new.record_id,
+                        ''{"action": "Update"}''
+                    );
+                END';
 
-        -- Inserarea datelor în tabela network_security_events
-            INSERT INTO network_security_events (event_id, device_id, interface_id, timestamp, event_type, description, status, resolved_at, threat_level, source_ip, destination_ip)
-            SELECT
-                LEVEL,
-                TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- device_id
-                TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- interface_id
-                TO_TIMESTAMP('2023-01-01 08:20:00', 'YYYY-MM-DD HH24:MI:SS') + DBMS_RANDOM.VALUE(1, 365), -- timestamp
-                'Event Type ' || LEVEL,
-                'Description ' || LEVEL,
-                CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'Open' ELSE 'Closed' END,
-                CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN TO_TIMESTAMP('2023-01-01 08:22:00', 'YYYY-MM-DD HH24:MI:SS') + DBMS_RANDOM.VALUE(1, 365) ELSE NULL END,
-                TRUNC(DBMS_RANDOM.VALUE(1, 5)), -- threat_level
-                '192.168.' || TO_CHAR(DBMS_RANDOM.VALUE(1, 255)) || '.' || TO_CHAR(DBMS_RANDOM.VALUE(1, 255)), -- source_ip
-                '192.168.' || TO_CHAR(DBMS_RANDOM.VALUE(1, 255)) || '.' || TO_CHAR(DBMS_RANDOM.VALUE(1, 255)) -- destination_ip
-            FROM DUAL
-            CONNECT BY LEVEL <= 10;
-
-        -- Inserarea datelor în tabela network_device_configurations
-            INSERT INTO network_device_configurations (configuration_id, device_id, timestamp, administrator_id, configuration_changes)
-            SELECT
-                LEVEL,
-                TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- device_id
-                TO_TIMESTAMP('2023-01-01 09:40:00', 'YYYY-MM-DD HH24:MI:SS') + DBMS_RANDOM.VALUE(1, 365), -- timestamp
-                TRUNC(DBMS_RANDOM.VALUE(1, 5)), -- administrator_id
-                '{"setting1": "value' || LEVEL || '", "setting2": "value' || LEVEL || '"}' -- configuration_changes
-            FROM DUAL
-            CONNECT BY LEVEL <= 10;
-
-        -- Inserarea datelor în tabela audit_logs
-            INSERT INTO audit_logs (audit_id, administrator_id, timestamp, action_type, table_affected, record_id, details)
-            SELECT
-                LEVEL,
-                TRUNC(DBMS_RANDOM.VALUE(1, 5)), -- administrator_id
-                TO_TIMESTAMP('2023-01-01 08:15:00', 'YYYY-MM-DD HH24:MI:SS') + DBMS_RANDOM.VALUE(1, 365), -- timestamp
-                CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'Create' ELSE 'Update' END,
-                CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'network_devices' ELSE 'network_interfaces' END,
-                TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- record_id
-                '{"detail1": "value' || LEVEL || '", "detail2": "value' || LEVEL || '"}' -- details
-            FROM DUAL
-            CONNECT BY LEVEL <= 10;
-    PROCEDURE simulate_traffic_and_alerts;
+            -- Create a trigger to log deletes
+            EXECUTE IMMEDIATE '
+                CREATE OR REPLACE TRIGGER audit_logs_delete_trigger
+                BEFORE DELETE ON ' || p_table_name || '
+                FOR EACH ROW
+                BEGIN
+                    INSERT INTO audit_logs (
+                        audit_id,
+                        administrator_id,
+                        timestamp,
+                        action_type,
+                        table_affected,
+                        record_id,
+                        details
+                    ) VALUES (
+                        audit_id_seq.NEXTVAL,
+                        :old.administrator_id,
+                        SYSTIMESTAMP,
+                        ''Delete'',
+                        ''' || p_table_name || ''',
+                        :old.record_id,
+                        ''{"action": "Delete"}''
+                    );
+                END';
+        END setup_audit_triggers;
+    PROCEDURE simulate_traffic_and_alerts AS
         BEGIN
             BEGIN
                 EXECUTE IMMEDIATE 'CREATE SEQUENCE alerta_seq';
@@ -364,37 +401,48 @@ CREATE OR REPLACE PACKAGE network_management AS
                     END IF;
                 END;
             END LOOP;
-        END;
-    PROCEDURE create_foreign_key_constraints AS
+        END simulate_traffic_and_alerts;
+    PROCEDURE create_and_insert_network_logs AS
         BEGIN
-        -- Adding Foreign Key Constraints
-            EXECUTE IMMEDIATE 'ALTER TABLE network_interfaces
-                ADD CONSTRAINT fk_network_interfaces_device
-                FOREIGN KEY (device_id) REFERENCES network_devices(device_id)';
+            -- Create Table
+            BEGIN
+                EXECUTE IMMEDIATE 'CREATE TABLE network_logs (
+                    log_id INT PRIMARY KEY,
+                    device_id INT,
+                    interface_id INT,
+                    timestamp TIMESTAMP,
+                    log_message VARCHAR2(255),
+                    log_level VARCHAR2(10),
+                    source_ip VARCHAR2(15)
+                )';
+            EXCEPTION
+                WHEN OTHERS THEN
+                    NULL; -- Table already exists, ignore the error
+            END;
 
-            EXECUTE IMMEDIATE 'ALTER TABLE network_logs
-                ADD CONSTRAINT fk_network_logs_device
-                FOREIGN KEY (device_id) REFERENCES network_devices(device_id)';
-
-            EXECUTE IMMEDIATE 'ALTER TABLE network_logs
-                ADD CONSTRAINT fk_network_logs_interface
-                FOREIGN KEY (interface_id) REFERENCES network_interfaces(interface_id)';
-
-            EXECUTE IMMEDIATE 'ALTER TABLE bandwidth_data
-                ADD CONSTRAINT fk_bandwidth_data_interface
-                FOREIGN KEY (interface_id) REFERENCES network_interfaces(interface_id)';
-
-            EXECUTE IMMEDIATE 'ALTER TABLE network_security_events
-                ADD CONSTRAINT fk_network_security_events_device
-                FOREIGN KEY (device_id) REFERENCES network_devices(device_id)';
-
-            EXECUTE IMMEDIATE 'ALTER TABLE network_security_events
-                ADD CONSTRAINT fk_network_security_events_interface
-                FOREIGN KEY (interface_id) REFERENCES network_interfaces(interface_id)';
-
-            EXECUTE IMMEDIATE 'ALTER TABLE network_device_configurations
-                ADD CONSTRAINT fk_network_device_configurations_device
-                FOREIGN KEY (device_id) REFERENCES network_devices(device_id)';
-        END;
+            -- Insert Sample Data
+            FOR i IN 1..10 LOOP
+                INSERT INTO network_logs (
+                    log_id,
+                    device_id,
+                    interface_id,
+                    timestamp,
+                    log_message,
+                    log_level,
+                    source_ip
+                )
+                SELECT
+                    ROWNUM + (i - 1) * 10, -- Generate unique log_id
+                    TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- device_id
+                    TRUNC(DBMS_RANDOM.VALUE(1, 10)), -- interface_id
+                    TO_TIMESTAMP('2023-01-01 08:05:00', 'YYYY-MM-DD HH24:MI:SS') + DBMS_RANDOM.VALUE(1, 365), -- timestamp
+                    'Log Message ' || ROWNUM + (i - 1) * 10,
+                    CASE WHEN DBMS_RANDOM.VALUE < 0.5 THEN 'Info' ELSE 'Error' END,
+                    '192.168.' || TO_CHAR(TRUNC(DBMS_RANDOM.VALUE(1, 255))) || '.' || TO_CHAR(TRUNC(DBMS_RANDOM.VALUE(1, 255))) -- source_ip
+                FROM DUAL
+                CONNECT BY LEVEL <= 10;
+            END LOOP;
+            COMMIT; -- Commit the changes
+        END create_and_insert_network_logs;
 END network_management;
 /
