7f5f106abd5bd5f5b9e473cb6e2f409b17bf8dca
AlbertMLD <albert.lungu@gmail.com>
2023-12-17 21:33:35 +0200

    Adding mew package

---
+++ CREATE OR REPLACE PACKAGE network_manage.sql
@@ -0,0 +1,190 @@
+CREATE OR REPLACE PACKAGE network_management AS
+    PROCEDURE create_network_devices_table;
+    PROCEDURE add_device(
+        p_device_id INT,
+        p_device_name VARCHAR(50),
+        p_device_ip_address VARCHAR(15),
+        p_device_type VARCHAR(20),
+        p_location VARCHAR(50),
+        p_manufacturer VARCHAR(50),
+        p_firmware_version VARCHAR(10),
+        p_status VARCHAR(10),
+        p_last_seen TIMESTAMP
+    );
+    PROCEDURE delete_device(p_device_id INT);
+    PROCEDURE insert_data;
+    PROCEDURE simulate_traffic_and_alerts;
+    PROCEDURE create_foreign_key_constraints;
+
+    -- Additional procedures for network_interfaces, network_logs, bandwidth_data, network_security_events, network_device_configurations
+    PROCEDURE add_interface(
+        p_interface_id INT,
+        p_device_id INT,
+        p_interface_name VARCHAR(50),
+        p_interface_type VARCHAR(20),
+        p_speed_mbps INT,
+        p_mac_address VARCHAR(12)
+    );
+    PROCEDURE add_log(
+        p_log_id INT,
+        p_device_id INT,
+        p_interface_id INT,
+        p_timestamp TIMESTAMP,
+        p_log_message VARCHAR(255),
+        p_log_level VARCHAR(10),
+        p_source_ip VARCHAR(15)
+    );
+    PROCEDURE add_bandwidth_data(
+        p_bandwidth_id INT,
+        p_interface_id INT,
+        p_timestamp TIMESTAMP,
+        p_incoming_bandwidth INT,
+        p_outgoing_bandwidth INT
+    );
+    PROCEDURE add_security_event(
+        p_event_id INT,
+        p_device_id INT,
+        p_interface_id INT,
+        p_timestamp TIMESTAMP,
+        p_event_type VARCHAR(50),
+        p_description VARCHAR(255),
+        p_status VARCHAR(10),
+        p_resolved_at TIMESTAMP,
+        p_threat_level INT,
+        p_source_ip VARCHAR(15),
+        p_destination_ip VARCHAR(15)
+    );
+    PROCEDURE add_device_configuration(
+        p_configuration_id INT,
+        p_device_id INT,
+        p_timestamp TIMESTAMP,
+        p_administrator_id INT,
+        p_configuration_changes VARCHAR(4000)
+    );
+END network_management;
+/
+
+CREATE OR REPLACE PACKAGE BODY network_management AS
+    PROCEDURE create_network_devices_table AS
+    BEGIN
+        BEGIN
+            EXECUTE IMMEDIATE 'CREATE TABLE network_devices (
+                device_id INT PRIMARY KEY,
+                device_name VARCHAR(50),
+                device_ip_address VARCHAR(15),
+                device_type VARCHAR(20),
+                location VARCHAR(50),
+                manufacturer VARCHAR(50),
+                firmware_version VARCHAR(10),
+                status VARCHAR(10),
+                last_seen TIMESTAMP
+            )';
+        EXCEPTION
+            WHEN OTHERS THEN
+                NULL; -- Table already exists, ignore the error
+        END;
+    END;
+
+    PROCEDURE add_device(
+        p_device_id INT,
+        p_device_name VARCHAR(50),
+        p_device_ip_address VARCHAR(15),
+        p_device_type VARCHAR(20),
+        p_location VARCHAR(50),
+        p_manufacturer VARCHAR(50),
+        p_firmware_version VARCHAR(10),
+        p_status VARCHAR(10),
+        p_last_seen TIMESTAMP
+    ) AS
+    BEGIN
+        EXECUTE IMMEDIATE 'INSERT INTO network_devices VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9)'
+        USING p_device_id, p_device_name, p_device_ip_address, p_device_type,
+              p_location, p_manufacturer, p_firmware_version, p_status, p_last_seen;
+    END;
+
+    PROCEDURE delete_device(p_device_id INT) AS
+    BEGIN
+        EXECUTE IMMEDIATE 'DELETE FROM network_devices WHERE device_id = :1' USING p_device_id;
+    END;
+
+    PROCEDURE insert_data AS
+    BEGIN
+        -- Add data insertion logic
+    END;
+
+    PROCEDURE simulate_traffic_and_alerts AS
+    BEGIN
+        -- Add traffic simulation logic
+    END;
+
+    PROCEDURE create_foreign_key_constraints AS
+    BEGIN
+        -- Add foreign key constraints
+    END;
+
+    -- Additional procedures for network_interfaces, network_logs, bandwidth_data, network_security_events, network_device_configurations
+    PROCEDURE add_interface(
+        p_interface_id INT,
+        p_device_id INT,
+        p_interface_name VARCHAR(50),
+        p_interface_type VARCHAR(20),
+        p_speed_mbps INT,
+        p_mac_address VARCHAR(12)
+    ) AS
+    BEGIN
+        -- Add logic for inserting data into network_interfaces table
+    END;
+
+    PROCEDURE add_log(
+        p_log_id INT,
+        p_device_id INT,
+        p_interface_id INT,
+        p_timestamp TIMESTAMP,
+        p_log_message VARCHAR(255),
+        p_log_level VARCHAR(10),
+        p_source_ip VARCHAR(15)
+    ) AS
+    BEGIN
+        -- Add logic for inserting data into network_logs table
+    END;
+
+    PROCEDURE add_bandwidth_data(
+        p_bandwidth_id INT,
+        p_interface_id INT,
+        p_timestamp TIMESTAMP,
+        p_incoming_bandwidth INT,
+        p_outgoing_bandwidth INT
+    ) AS
+    BEGIN
+        -- Add logic for inserting data into bandwidth_data table
+    END;
+
+    PROCEDURE add_security_event(
+        p_event_id INT,
+        p_device_id INT,
+        p_interface_id INT,
+        p_timestamp TIMESTAMP,
+        p_event_type VARCHAR(50),
+        p_description VARCHAR(255),
+        p_status VARCHAR(10),
+        p_resolved_at TIMESTAMP,
+        p_threat_level INT,
+        p_source_ip VARCHAR(15),
+        p_destination_ip VARCHAR(15)
+    ) AS
+    BEGIN
+        -- Add logic for inserting data into network_security_events table
+    END;
+
+    PROCEDURE add_device_configuration(
+        p_configuration_id INT,
+        p_device_id INT,
+        p_timestamp TIMESTAMP,
+        p_administrator_id INT,
+        p_configuration_changes VARCHAR(4000)
+    ) AS
+    BEGIN
+        -- Add logic for inserting data into network_device_configurations table
+    END;
+END network_management;
+/
